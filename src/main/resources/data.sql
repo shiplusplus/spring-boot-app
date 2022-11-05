37200da2cf312839e02ebcb6d37b87d4c28091b2
s0g09hf <shivangi.gupta0@walmart.com>
2022-11-05 16:00:32 +0530

    minimal data; application working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,6 @@ INSERT INTO student (student_id, student_name) VALUES (1000, 'Shivangi');
 
 INSERT INTO course (course_id, course_name, credits) VALUES (1, 'OOP', 4);
 
+INSERT INTO marks (marks_id, course_id, total_marks, student_marks, grade) VALUES (1, 1,100,95,9);
+
 
