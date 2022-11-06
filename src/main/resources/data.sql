ddb5a811c24d30f4f8c8b42043e3d1e274eea73d
s0g09hf <shivangi.gupta0@walmart.com>
2022-11-06 14:06:10 +0530

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,23 @@
 
-INSERT INTO student (student_id, student_name) VALUES (1000, 'Shivangi');
+INSERT INTO student (student_id, student_name) VALUES (0026, 'Shivangi');
+INSERT INTO student (student_id, student_name) VALUES (0027, 'Sagar');
 
-INSERT INTO course (course_id, course_name, credits) VALUES (1, 'OOP', 4);
+INSERT INTO semester (semester_id, student_id) VALUES (5,0026);
+INSERT INTO semester (semester_id, student_id) VALUES (6,0026);
 
-INSERT INTO marks (marks_id, course_id, total_marks, student_marks, grade) VALUES (1, 1,100,95,9);
+INSERT INTO course (course_id, course_name, credits) VALUES (101, 'OOP', 4);
+INSERT INTO course (course_id, course_name, credits) VALUES (102, 'DSA', 4);
+INSERT INTO course (course_id, course_name, credits) VALUES (103, 'DBMS', 4);
+
+INSERT INTO marks (marks_id, semester_id, course_id, grade) VALUES (1,5, 101,9);
+INSERT INTO marks (marks_id, semester_id, course_id, grade) VALUES (2,5, 101,9);
+INSERT INTO marks (marks_id, semester_id, course_id, grade) VALUES (3,5, 101,9);
+INSERT INTO marks (marks_id, semester_id, course_id, grade) VALUES (4,5, 101,9);
+
+
+
+
+
+
 
 
